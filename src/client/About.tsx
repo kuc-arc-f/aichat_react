1e3e2e725ce2eb8538a0fbd0eece7f8480a441c8
_ñ <knaka0209@gmail.com>
2024-05-19 07:23:29 +0900

    up, 0519a3

--- src/client/About.tsx
+++ src/client/About.tsx
@@ -1,9 +1,8 @@
-import Head from '../components/Head'
+//import Head from '../components/Head'
 //
 function About() {
   return (
   <div className="container mx-auto my-2 px-8 bg-white">
-    <Head />
     <h1 className="text-4xl text-gray-700 font-bold my-2"
     >About!</h1>
   </div>
--- src/client/Home.tsx
+++ src/client/Home.tsx
@@ -2,6 +2,7 @@ import {useState, useEffect}  from 'react';
 import React from 'react'
 
 import Head from '../components/Head'
+import LoadBox from '../components/LoadBox';
 import { marked } from 'marked';
 import CrudIndex from './Home/CrudIndex';
 //
@@ -21,6 +22,7 @@ function Home() {
     try {
       initDisplay = true;
       answer = "";
+      setUpdatetime(new Date().toString() + String(Math.random()));
       const result = await CrudIndex.addItem();
   console.log(result);
       initDisplay = false;
@@ -30,7 +32,7 @@ function Home() {
         //@ts-ignore 
         answer = s 
       } 
-      setUpdatetime(new Date().toString())
+      setUpdatetime(new Date().toString() + String(Math.random()));
     } catch (error) {
       initDisplay = false;
       console.error(error);
@@ -54,30 +56,36 @@ function Home() {
   }
   //
   return (
-  <div className="container mx-auto my-2 px-8 bg-white">
-    <Head />
-    <h1 className="text-4xl font-bold">AI-Chat!</h1>
-    <hr className="my-2"></hr>
-    <div className="flex flex-row">
-      <div className="flex-1 text-center p-1">
-        <textarea
-         className="border border-gray-400 rounded-md px-3 py-2 w-full resize-none focus:outline-none focus:border-blue-500"
-         name="input_text" id="input_text" rows="3" />
+  <>
+    {initDisplay ? (<LoadBox />) : null}
+    <div className="container mx-auto my-2 px-8 bg-white">
+      <h1 className="text-4xl font-bold">AI-Chat!</h1>
+      <hr className="my-2"></hr>
+      <div className="flex flex-row">
+        <div className="flex-1 text-center p-1">
+          <textarea
+          className="border border-gray-400 rounded-md px-3 py-2 w-full resize-none focus:outline-none focus:border-blue-500"
+          name="input_text" id="input_text" rows="3" />
+        </div>
+        <div className="text-center pt-1">
+          <button 
+          className="bg-blue-600 text-white font-bold ms-2 py-2 px-4 rounded"
+          onClick={()=>sendText()}>Send</button>
+          <br />
+          <button 
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white ms-2 mt-2 py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={()=>clearText()}>Clear</button>
+        </div>
       </div>
-      <div className="text-center pt-1">
-        <button 
-        className="bg-blue-600 text-white font-bold ms-2 py-2 px-4 rounded"
-        onClick={()=>sendText()}>Send</button>
-        <br />
-        <button 
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white ms-2 mt-2 py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        onClick={()=>clearText()}>Clear</button>
-      </div>
+      <hr className="my-2" />
+      answer :<br />
+      {answer ? (
+        <div className="bg-sky-100 p-2 rounded"
+        dangerouslySetInnerHTML={{ __html: answer}} />
+      ) : null}
     </div>
-    <hr className="my-2" />
-    answer :<br />
-    <div dangerouslySetInnerHTML={{ __html: answer}} />
-  </div>
+  </>
+
   )
 }
 
